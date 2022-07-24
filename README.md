80f3725478e9bcf76c80a2aaf76cc16fed03bad6
Ryan Perry <ricoolohsix@hotmail.com>
2022-07-24 17:35:35 +0100

    feat: create script that can be used for backing up a Squad ServerConfig folder

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # hntp-squad-server
 Config and other guff that needs to be stored remotely so we can run administer the Squad server with Pterodactyl Panel
+
+# How to use
+1. [Make a Github app](https://docs.github.com/en/developers/apps) with the permissions scope set to a single file: `backup/squadserverbackup.tar.gz.gpg`
+
+1. Create and download a private key for you github app.
+
+1. Install the Github app in your chosen repo.
+
+1. Your Github app RSA private key (usually a .pem file) should be stored in `./secrets/<private-key-file>.pem`
+
+1. The following env vars need to be set for `./backup.sh` to run:
+    - SERVERCONFIG_FOLDER: the location of the Squad `ServerConfig` folder (usually something like `/home/steam/squad-dedicated/SquadGame/SquadServer`)
+    - GITHUB_APP_KEY_FILENAME: the path to the github app RSA private key (e.g. `./secrets/hntp-config-manager.pem`)
+    - BACKUP_ENCRYPTION_PASSPHRASE: the passphrase with which to encrypt the backup `.tar.gz` file that stores the contents of the `ServerConfig` folder (make it a strong password if you can)
+
+1. Once these variables have been set, you should be able to run `./backup.sh` and the download url and instructions for decryption will be displayed.
+
+# Requirements
+- `base64url` (can be installed with `sudo apt install basez`)
+- `jq` (can be installed with `sudo apt install jq`)
+- `openssl`
+- `curl`
+- `gpg`
---
+++ backup.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+
+test -d ${SERVERCONFIG_FOLDER} || { echo "ServerConfig folder not found. (try setting env var SERVERCONFIG_FOLDER)"; exit 1; }
+test -f ${GITHUB_APP_KEY_FILENAME} || { echo "Github app key file not found. Could not backup server config. (try setting env var GITHUB_APP_KEY_FILENAME)"; exit 1; }
+test -n ${BACKUP_ENCRYPTION_PASSPHRASE} || { echo "Passphrase not set for backup file. (try setting env var BACKUP_ENCRYPTION_PASSPHRASE)"; exit 1; }
+
+backup_content=$(tar czf - ${SERVERCONFIG_FOLDER}/* | gpg --symmetric --cipher-algo aes256 --batch --passphrase ${BACKUP_ENCRYPTION_PASSPHRASE} | base64 -w 0)
+
+# Backup via github API
+now=$(date +%s)
+
+jwt_header=$(cat <<EOF |
+{
+  "alg": "RS256",
+  "typ": "JWT"
+}
+EOF
+jq -Mcj '.' | base64 - | tr -d '=')
+
+jwt_claims=$(cat <<EOF |
+{
+  "iat": $((${now} - 60)),
+  "exp": $((${now} + (60 * 10))),
+  "iss": "222321"
+}
+EOF
+jq -Mcj '.' | base64 - | tr -d '=')
+
+jwt_payload=${jwt_header}.${jwt_claims}
+
+jwt_signature=$(printf '%s' "${jwt_payload}" | openssl dgst -sha256 -sign "${GITHUB_APP_KEY_FILENAME}" -binary | base64url -w 0 - | tr -d '=')
+
+jwt=${jwt_header}.${jwt_claims}.${jwt_signature}
+
+echo "Fetching Github app installation ID"
+installation_id=$(curl -s -H "Authorization: Bearer ${jwt}" -H "Accept: application/vnd.github+json" https://api.github.com/app/installations | \
+jq -c '.[] | select(.app_slug | contains("hntp-squad-server-config-manager")) | .id')
+
+echo "Fetching Github app installation auth token"
+installation_auth_token=$(curl -s -X POST \
+-H "Authorization: Bearer ${jwt}" \
+-H "Accept: application/vnd.github+json" \
+"https://api.github.com/app/installations/${installation_id}/access_tokens" | jq -cr '.token')
+
+echo "Fetching existing backup sha"
+existing_backup_sha=$(curl -s \
+  -X GET \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: token ${installation_auth_token}" \
+  https://api.github.com/repos/Ricool06/hntp-squad-server/contents/backup/squadserverbackup.tar.gz.gpg | jq -cr '.sha')
+
+echo "Pushing backup to repo"
+backup_response=$(curl \
+  -X PUT \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: token ${installation_auth_token}" \
+  https://api.github.com/repos/Ricool06/hntp-squad-server/contents/backup/squadserverbackup.tar.gz.gpg \
+  -d @- <<- EOF
+{
+  "message":"my commit message",
+  "committer": {
+    "name":"Brick",
+    "email":"brick@ricool.uk"
+  },
+  "content":"${backup_content}",
+  "sha":"${existing_backup_sha}"
+}
+EOF
+)
+
+download_url=$(echo ${backup_response} | jq -c '.content.download_url')
+
+echo "Backup created. Download from ${download_url}"
+echo 'Then decrypt the backup file with: gpg --batch --passphrase ${BACKUP_ENCRYPTION_PASSPHRASE} -d ./squadserverbackup.tar.gz.gpg | tar xzf -'
